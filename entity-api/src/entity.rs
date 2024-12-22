754c446c8723177821ab354ab850debce26c7676
Yi-Jyun Pan <pan93412@gmail.com>
2024-12-22 11:21:21 +0800

    chore: More meanful "url" field name in Entity

--- entity-api/src/entity.rs
+++ entity-api/src/entity.rs
@@ -26,16 +26,13 @@ static EXPIRE_AT: Duration = Duration::from_secs(3600);
 
 #[async_graphql::ComplexObject]
 impl Entity {
-    /// Get the image of the entity.
+    /// Get the image URL of the entity.
     ///
     /// Note that it expires in 1 hour. Therefore, you should not save this URL.
     /// Always gets the image URL from the entity.
     ///
     /// If the URL expires, you might need to fetch the entity again to get a new URL.
-    pub async fn image(
-        &self,
-        context: &async_graphql::Context<'_>,
-    ) -> async_graphql::Result<String> {
+    pub async fn url(&self, context: &async_graphql::Context<'_>) -> async_graphql::Result<String> {
         let storage = context.data::<Storage>()?;
         let path = format!("/{}", self.image_id);
         let image = storage.presign_read(&path, EXPIRE_AT).await?;
