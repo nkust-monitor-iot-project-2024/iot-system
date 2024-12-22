0e7ec5edd96859ba8c6876fad09ce9728e7a3329
Yi-Jyun Pan <pan93412@gmail.com>
2024-12-22 23:46:03 +0800

    refactor(entity-api): Update to use Chrono types

--- entity-api/src/entity.rs
+++ entity-api/src/entity.rs
@@ -2,7 +2,6 @@ use std::time::Duration;
 
 use crate::{query::Monitor, storage::Storage};
 use async_graphql::SimpleObject;
-use sqlx::types::BigDecimal;
 
 /// A detected entity.
 #[derive(SimpleObject)]
@@ -17,12 +16,12 @@ pub struct Entity {
     /// The confidence of the entity.
     ///
     /// It should be in the range of 0.0 to 1.0.
-    pub confidence: BigDecimal,
+    pub confidence: bigdecimal::BigDecimal,
     /// The monitor of the entity.
     #[graphql(skip)]
     pub monitor_id: Option<String>,
     /// The time when the entity was detected.
-    pub created_at: time::PrimitiveDateTime,
+    pub created_at: chrono::DateTime<chrono::Utc>,
 }
 
 static EXPIRE_AT: Duration = Duration::from_secs(3600);
